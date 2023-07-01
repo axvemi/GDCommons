9e0c6e5f71a96c6b71787b4143602ab5ca6952f9
Axel <axvemi@gmail.com>
2023-07-02 00:42:05 +0200

    Get Recursive Children

--- GDUtils.cs
+++ GDUtils.cs
@@ -1,8 +1,27 @@
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using Godot;
 
 namespace Axvemi.Commons;
 
 public static class GDUtils
 {
 	public static string GetStrippedBbCode(string text) => Regex.Replace(text, "\\[.+?\\]", "");
+
+	public static List<Node> GetRecursiveChildren(Node node, bool includeRoot = false)
+	{
+		List<Node> allChildren = new();
+		if (includeRoot)
+		{
+			allChildren.Add(node);
+		}
+
+		foreach (Node child in node.GetChildren())
+		{
+			allChildren.Add(child);
+			allChildren.AddRange(GetRecursiveChildren(child));
+		}
+		
+		return allChildren;
+	}
 }
